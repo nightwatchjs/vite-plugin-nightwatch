37538a0f603a6b9dfb781a3c8e432a8bb22fe04f
Ravi Sawlani <ravisawlani04@gmail.com>
2022-01-14 17:14:29 +0530

    refactoring

--- nightwatch/commands/mountReactComponent.js
+++ nightwatch/commands/mountReactComponent.js
@@ -1,41 +1,48 @@
 module.exports = class Command {
     async command(componentName, props, cb = function() {}) {
-  
-      const scriptFn = function(componentName, props) {
+
+        let scriptContent = `
+            import Component from '${componentName}'
+            var reactElement = React.createElement(Component)
+            let element = ReactDOM.render(reactElement, document.getElementById('app'));
+            window['@component_element'] = element;
+            window['@component_class'] = Component;
+            console.log('component element', reactElement)
+            console.log('Component', Component)
+            `
+    
+      const scriptFn = function(scriptContent) {
         var scriptEl = document.createElement('script');
         scriptEl.type = 'module';
-        scriptEl.innerHTML = `
-        import Component from '${componentName}'
-        var reactElement = React.createElement(Component, ${props})
-        ReactDOM.render(reactElement, document.getElementById('app'));`;
+        scriptEl.innerHTML = scriptContent;
         document.body.appendChild(scriptEl);
       }
 
-      const {debuggerAddress} = this.api.capabilities['goog:chromeOptions'] || {};
-      let wsUrl;
-      if (debuggerAddress) {
+        const {debuggerAddress} = this.api.capabilities['goog:chromeOptions'] || {};
+        let wsUrl;
+        if (debuggerAddress) {
         const address = debuggerAddress.split(':');
         const request = await this.httpRequest({
-          host: address[0],
-          port: address[1],
-          path: '/json',
-          method: 'GET'
+            host: address[0],
+            port: address[1],
+            path: '/json',
+            method: 'GET'
         });
-  
+
         wsUrl = request.filter(item => {
-          return item.type === 'page'
+            return item.type === 'page'
         })[0].webSocketDebuggerUrl;
-      }
-  
-      let wsUrlSection = '';
-      if (wsUrl) {
-        //wsUrlSection = '?wsurl=' + encodeURIComponent(wsUrl);
-      }
+        }
+
+        let wsUrlSection = '';
+        if (wsUrl) {
+            //wsUrlSection = '?wsurl=' + encodeURIComponent(wsUrl);
+        }
   
       const renderedElement = await this.api
         .navigateTo('/test_render/')
         .pause(2000)
-        .execute(scriptFn, [componentName, props])
+        .execute(scriptFn, [scriptContent])
         .pause(500)
         .execute(function() {
           return document.querySelectorAll('#app')[0].firstChild
