1c0ec5db94794dc973a1efe6c46349f45a92ccf5
Vaibhav Singh <vaibhav.s@browserstack.com>
2023-04-20 20:08:44 +0530

    Add: types and update dependencies to latest versions (#10)

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+import { NightwatchAPI, Element } from 'nightwatch';
+import { MountingOptions } from '@vue/test-utils';
+
+type GlobalMountOptions = NonNullable<MountingOptions<any>['global']>;
+
+declare module 'nightwatch' {
+  interface NightwatchAPI {
+    importScript(
+      scriptPath: string,
+      options: { scriptType: string; componentType: string },
+      callback?: () => void
+    ): this;
+    mountReactComponent<TProps extends Record<string, any>>(
+      componentPath: string,
+      props?: TProps,
+      callback?: (this: NightwatchAPI, result: Element) => void
+    ): Awaitable<this, Element>;
+    mountVueComponent(
+      componentPath: string,
+      options?: {
+        props?: Pick<MountingOptions<any>, 'props'>;
+        plugin?: Pick<GlobalMountOptions, 'plugins'>;
+        mocks?: Pick<GlobalMountOptions, 'mocks'>;
+      },
+      callback?: (this: NightwatchAPI, result: Element) => void
+    ): Awaitable<this, Element>;
+    launchComponentRenderer(): this;
+  }
+}
