bc1d0aa7a5372a036ede449eada019bfd3c81622
Vaibhav Singh <vaibhav.s@browserstack.com>
2023-04-20 19:34:16 +0530

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
 import { NightwatchAPI, Element } from 'nightwatch';
+import { MountingOptions } from '@vue/test-utils';
 type RecordObject = Record<string, any>;
+
+type GlobalMountOptions = NonNullable<MountingOptions<any>['global']>;
 
 declare module 'nightwatch' {
   interface NightwatchAPI {
@@ -15,7 +18,11 @@ declare module 'nightwatch' {
     ): Awaitable<this, Element>;
     mountVueComponent(
       componentPath: string,
-      options?: { plugin?: RecordObject; mocks?: RecordObject },
+      options?: {
+        props?: Pick<MountingOptions<any>, 'props'>;
+        plugin?: Pick<GlobalMountOptions, 'plugins'>;
+        mocks?: Pick<GlobalMountOptions, 'mocks'>;
+      },
       callback?: (this: NightwatchAPI, result: Element) => void
     ): Awaitable<this, Element>;
     launchComponentRenderer(): this;
